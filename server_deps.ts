beb91c217abcc67c9618ba5319be8f4e4ee49fa1
xuybin <xuybin@qq.com>
2022-04-08 15:47:20 +0800

    refactor:

--- server_deps.ts
+++ server_deps.ts
@@ -1,4 +1,4 @@
-/// <reference types="https://deno.land/x/xuybin_fresh@v0.0.5/types.d.ts" />
-export * from "https://deno.land/x/xuybin_fresh@v0.0.5/server.ts";
+/// <reference types="https://deno.land/x/xuybin_fresh@v0.0.6/types.d.ts" />
+export * from "https://deno.land/x/xuybin_fresh@v0.0.6/server.ts";
 
 export { virtualSheet } from "https://esm.sh/twind@0.16.16/sheets";
